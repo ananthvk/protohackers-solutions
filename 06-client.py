a0f89e72414b7e9d34b14a8beff4f065a9c1829d
Ananth <mailstoanantha@gmail.com>
2023-11-21 18:54:40 +0530

    finished most features

--- 06-client.py
+++ 06-client.py
@@ -5,26 +5,38 @@ async def main():
     creader, cwriter = await asyncio.open_connection("127.0.0.1", 8000)
     # Camera client
     cwriter.write(bytearray([0x80, 0x00, 0x7B, 0x00, 0x08, 0x00, 0x3C]))
-    # Send a plate to the server
+    # Send a request for hearbeat
     cwriter.write(bytearray([0x40, 0x00, 0x00, 0x00, 0x0A]))
-    while True:
-        message = await creader.read(4096)
-        print(message)
-
-    # cwriter.write(
-    #    bytearray([0x20, 0x04, 0x55, 0x4E, 0x31, 0x58, 0x00, 0x00, 0x00, 0x00])
-    # )
+    # Send a plate to the server
+    cwriter.write(
+        bytearray([0x20, 0x04, 0x55, 0x4E, 0x31, 0x58, 0x00, 0x00, 0x00, 0x00])
+    )
     # cwriter.write(bytearray([0x81, 0x01, 0x00, 0x7B]))
-    try:
-        async with asyncio.timeout(1):
-            message = await creader.read(4096)
-        print(message[2:].decode("utf-8"))
-    except:
-        pass
     await cwriter.drain()
     cwriter.close()
-
     await cwriter.wait_closed()
+
+    c2reader, c2writer = await asyncio.open_connection("127.0.0.1", 8000)
+    # Camera client
+    c2writer.write(bytearray([0x80, 0x00, 0x7B, 0x00, 0x09, 0x00, 0x3C]))
+    # Send a request for hearbeat
+    c2writer.write(bytearray([0x40, 0x00, 0x00, 0x00, 0x0A]))
+    # Send a plate to the server
+    c2writer.write(
+        bytearray([0x20, 0x04, 0x55, 0x4E, 0x31, 0x58, 0x00, 0x00, 0x00, 0x2d])
+    )
+    # c2writer.write(bytearray([0x81, 0x01, 0x00, 0x7B]))
+    await c2writer.drain()
+    c2writer.close()
+    await c2writer.wait_closed()
+    
+    # Get a dispatcher
+    dreader, dwriter = await asyncio.open_connection("127.0.0.1", 8000)
+    # Dispatcher client
+    dwriter.write(bytearray([0x81, 0x01, 0x00, 0x7b]))
+    await dwriter.drain()
+    dwriter.close()
+    await dwriter.wait_closed()
 
 
 asyncio.run(main())
--- 06-speed-daemon.py
+++ 06-speed-daemon.py
@@ -1,8 +1,21 @@
+import os
+from collections import deque 
+
+os.environ["PYTHONASYNCIODEBUG"] = "1"
+import logging
 import struct
 import asyncio
 import traceback
 from datetime import datetime
 
+# Global data store
+
+# Contains a mapping road->dispatchers
+dispatchers = dict()
+# Contains mapping plate: mile, timestamp
+plates = dict()
+# Ticket queue: road -> ticket (as bytes)
+tickets = dict()
 encoding = "utf-8"
 
 
@@ -118,7 +131,7 @@ class TicketWriter:
                 timestamp1,
                 mile2,
                 timestamp2,
-                speed * 100,
+                int(speed * 100),
             )
         )
         await self.writer.drain()
@@ -146,14 +159,17 @@ class HeartBeatTask:
         # So handle exceptions here
         try:
             while True:
+                if self.client.closed:
+                    break
                 log(self.client.writer, "Sending hearbeat to client")
                 self.writer.write(bytearray([0x41]))
                 await self.writer.drain()
                 await asyncio.sleep(self.interval / 10)
-        except:
+        except Exception as e:
             log(
                 self.client.writer,
                 "Error while sending hearbeat ... closing connection",
+                traceback.format_exc(),
             )
         finally:
             await self.client.close()
@@ -170,16 +186,26 @@ class HeartBeatTask:
         self.client.tasks.add(self.task)
         self.task.add_done_callback(self.client.tasks.discard)
 
-
 class CameraHandler:
-    def __init__(self, client: Client) -> None:
+    def __init__(self, client: Client, heartbeat_set) -> None:
         self.reader = client.reader
         self.client = client
         self.readers = {
             # OP code   # Reader
             0x20: PlateReader,
-            0x40: HeartBeatReader,
         }
+        if not heartbeat_set:
+            self.readers = {
+                # OP code   # Reader
+                0x20: PlateReader,
+                0x40: HeartBeatReader,
+            }
+        else:
+            # Hearbeat has already been set, should not accept anymore hearbeat responses
+            self.readers = {
+                # OP code   # Reader
+                0x20: PlateReader,
+            }
 
     async def handle(self):
         log(self.client.writer, "The client has identified itself as a camera")
@@ -198,7 +224,47 @@ class CameraHandler:
                 # We have gotten a plate
                 parsed = await instance.read()
                 if opcode == 0x20:
-                    pass
+                    plate = parsed[0]
+                    timestamp = parsed[1]
+                    log(self.client.writer, f"Got Plate {{{plate} at {timestamp}}}")
+                    if plates.get(plate) is None:
+                        # This is the first plate we have received
+                        plates[plate] = [(mile, timestamp)]
+                    else:
+                        plates[plate].append((mile, timestamp))
+                        plates[plate].sort()
+                        log(self.client.writer, f"There is information about the client, checking if a ticket is needed")
+                        for i in range(1, len(plates[plate])):
+                            # Check adjacent observations
+                            mile2 = plates[plate][i][0] 
+                            mile1 = plates[plate][i-1][0]
+                            timestamp2 = plates[plate][i][1] 
+                            timestamp1 = plates[plate][i-1][1]
+                            distance =  mile2 - mile1 
+                            t = timestamp2 - timestamp1 
+                            v = (distance/t)*60*60 # Convert miles/s to miles/h
+                            if abs(v) - limit >= 0.5:
+                                # The client was overspeeding
+                                log(self.client.writer, f"{plate} was overspeeding with speed {v}")
+                                road_dispatchers = dispatchers.get(road)
+                                ticket = None #TODO
+
+                                if road_dispatchers is None:
+                                    # There are no active dispatchers for this road, wait for a one to connect
+                                    log(self.client.writer, f"There are no active dispatchers for {road}, waiting")
+                                    if tickets.get(road) is None:
+                                        tickets[road] = deque()
+                                    tickets[road].append((plate, road, mile1, timestamp1, mile2, timestamp2, v))
+                                else:
+                                    for dispatcher in road_dispatchers:
+                                        if not dispatcher.client.closed:
+                                            # Send the ticket
+                                            log(self.client.writer, f"There was an active dispatcher, sending ticket")
+                                            await TicketWriter(dispatcher.client).write(plate, road, mile1, timestamp1, mile2, timestamp2, v)
+                                        
+                                        
+                                
+
                 if opcode == 0x40:
                     # Remove the heart beat reader from self.readers
                     # So that any more heart beat requests from this client will be an error
@@ -211,15 +277,57 @@ class CameraHandler:
 
 
 class DispatcherHandler:
-    def __init__(self, client: Client) -> None:
+    def __init__(self, client: Client, hearbeat_set) -> None:
         self.client = client
         self.reader = client.reader
+        self.readers = {0x40: HeartBeatReader}
+        self.tasks = set()
+        if not hearbeat_set:
+            self.readers = {0x40: HeartBeatReader}
+        else:
+            self.readers = dict()
 
     async def handle(self):
+        log(self.client.writer, "The client has identifed itself as a dispatcher")
         num_roads = await self.reader.readexactly(1)
         num_roads = num_roads[0]
         roads = await self.reader.read(2 * num_roads)
-        return struct.unpack(f"!{num_roads}H", roads)
+        roads = struct.unpack(f"!{num_roads}H", roads)
+        for road in roads:
+            if not dispatchers.get(road):
+                dispatchers[road] = [self.client]
+            else:
+                dispatchers[road].append(self.client)
+            if tickets.get(road) is not None:
+                log(self.client.writer, "A pending ticket for this road was found, sending it")
+                # task = asyncio.create_task(TicketWriter(self.client).write(*tickets[road].pop()))
+                # self.tasks.add(task)
+                # task.add_done_callback(self.tasks.discard)
+                try:
+                    while len(tickets[road]) != 0:
+                        ticket = tickets[road].popleft()
+                        await TicketWriter(self.client).write(*ticket)
+                except:
+                    log(self.client.writer, "ERROR, couldn't send ticket", traceback.format_exc())
+
+        log(self.client.writer, f"Dispatcher {{roads: {roads}}}")
+        while True:
+            # The client has identified itself as a dispatcher
+            # The only valid client request is WantHeartbeat
+            opcode = await self.reader.read(1)
+            if opcode:
+                opcode = opcode[0]
+                instance = self.readers[opcode](self.client)
+                parsed = await instance.read()
+                if opcode == 0x40:
+                    # Remove the heart beat reader from self.readers
+                    # So that any more heart beat requests from this client will be an error
+                    del self.readers[0x40]
+                    await HeartBeatTask(self.client, parsed).start()
+            else:
+                # The client disconnected
+                log(self.client.writer, "Disconnected")
+                return
 
 
 class SpeedDaemonServer:
@@ -237,9 +345,17 @@ class SpeedDaemonServer:
         client = Client(reader, writer, self.loop)
         try:
             opcode = await reader.read(1)
+            heartbeat_set = False
             if opcode:
                 opcode = opcode[0]
-                instance = self.handlers[opcode](client)
+                # Check if it is a hearbeat request
+                if opcode == 0x40:
+                    heartbeat_set = True
+                    parsed = await HeartBeatReader(client).read()
+                    await HeartBeatTask(client, parsed).start()
+                    opcode = (await reader.read(1))[0]
+
+                instance = self.handlers[opcode](client, heartbeat_set)
                 await instance.handle()
         except Exception as e:
             log(writer, f"ERROR: {traceback.format_exc()}")
@@ -255,6 +371,11 @@ class SpeedDaemonServer:
 
 async def main():
     loop = asyncio.get_running_loop()
+    # Remove these in production
+    logging.basicConfig(level=logging.DEBUG)
+    loop.set_debug(True)
+    logging.getLogger("asyncio").setLevel(logging.DEBUG)
+    # ======
     sds = SpeedDaemonServer(loop)
     server = await asyncio.start_server(
         sds.handle_connection, host="0.0.0.0", port=8000
@@ -264,4 +385,5 @@ async def main():
         await server.serve_forever()
 
 
-asyncio.run(main())
+asyncio.run(main(), debug=True)
+# asyncio.run(main())
