dd34f5994151971ffbf67c950caeab75dfa79aa7
Ananth <mailstoanantha@gmail.com>
2023-11-22 15:06:23 +0530

    Fixed bug related to multiple tickets sent

--- 06-speed-daemon.py
+++ 06-speed-daemon.py
@@ -1,6 +1,7 @@
 import os
 import math
-from collections import deque 
+from collections import deque
+import itertools
 
 os.environ["PYTHONASYNCIODEBUG"] = "1"
 import logging
@@ -13,31 +14,44 @@ from datetime import datetime
 
 # Contains a mapping road->dispatchers
 dispatchers = dict()
-# Contains mapping plate: mile, timestamp
-plates = dict()
-# Ticket queue: road -> ticket (as bytes)
-tickets = dict()
-# last ticket sent
-last_ticket = dict()
-encoding = "utf-8"
+"""
+{
+    road_number: {dispatcher1, dispatcher2, ....}
+}
+"""
 
+plates = dict()
+pending_tickets = dict()
+"""
+{
+    road_number: {
+        'PLATE': {Ticket, Ticket, Ticket}
+    }
+}
+"""
+
+last_tickets = dict()
+"""
+{
+   'PLATE': {1, 2, 3} # Days for which a ticket was sent
+}
+"""
+encoding = "utf-8"
 
 def info(writer, *args, **kwargs):
     addr = writer.get_extra_info("peername")
     addr = f"{addr[0]}:{addr[1]}"
     print(f"{datetime.now().strftime('%T.%f')} [{addr}]", *args, **kwargs)
-
-
 def log(writer, *args, **kwargs):
     addr = writer.get_extra_info("peername")
     addr = f"{addr[0]}:{addr[1]}"
     print(f"{datetime.now().strftime('%T.%f')} [{addr}]", *args, **kwargs)
-
-
+def log3(writer, *args, **kwargs):
+    addr = writer.get_extra_info("peername")
+    addr = f"{addr[0]}:{addr[1]}"
+    print(f"{datetime.now().strftime('%T.%f')} [{addr}]", *args, **kwargs)
 def log2(*args, **kwargs):
     print(f"{datetime.now().strftime('%T.%f')} [INFO]", *args, **kwargs)
-
-
 class Client:
     def __init__(
         self,
@@ -66,12 +80,9 @@ class Client:
             for task in self.tasks:
                 try:
                     task.cancel()
-                    log2(addr, "Cancelled a task")
+                    # log2(addr, "Cancelled a task")
                 except:
                     pass
-
-
-# Sends an error message to the client and also disconnect the client
 class ErrorWriter:
     def __init__(self, client: Client) -> None:
         self.client = client
@@ -90,8 +101,6 @@ class ErrorWriter:
                     await self.writer.wait_closed()
                 except:
                     await self.client.close()
-
-
 class PlateReader:
     def __init__(self, client: Client) -> None:
         self.client = client
@@ -104,14 +113,11 @@ class PlateReader:
         plate = await self.reader.readexactly(plate_length + 4)
         plate, timestamp = struct.unpack(f"!{plate_length}sI", plate)
         return plate, timestamp
-
-
-class TicketWriter:
+class TicketPacker:
     def __init__(self, client: Client) -> None:
         self.client = client
-        self.writer = client.writer
 
-    async def write(
+    def pack(
         self,
         plate: bytes,
         road: int,
@@ -120,11 +126,10 @@ class TicketWriter:
         mile2: int,
         timestamp2: int,
         speed: int,
-    ) -> None:
+    ):
         plate_length = len(plate)
-        log(self.writer, f"Writing ticket: {plate_length} {plate} {road} {mile1} {timestamp1} {mile2} {timestamp2} {int(speed*100)}")
-        self.writer.write(
-            struct.pack(
+        log(self.client.writer, f"Packing ticket: {plate_length} {plate} {road} {mile1} {timestamp1} {mile2} {timestamp2} {int(speed*100)}")
+        return struct.pack(
                 f"!BB{plate_length}sHHIHIH",
                 0x21,
                 plate_length,
@@ -136,10 +141,6 @@ class TicketWriter:
                 timestamp2,
                 int(speed * 100),
             )
-        )
-        await self.writer.drain()
-
-
 class HeartBeatReader:
     def __init__(self, client: Client) -> None:
         self.client = client
@@ -148,8 +149,6 @@ class HeartBeatReader:
     async def read(self) -> int:
         interval = await self.reader.readexactly(4)
         return struct.unpack("!I", interval)[0]
-
-
 class HeartBeatTask:
     def __init__(self, client: Client, interval: int) -> None:  # In deciseconds
         self.client = client
@@ -189,6 +188,91 @@ class HeartBeatTask:
         self.client.tasks.add(self.task)
         self.task.add_done_callback(self.client.tasks.discard)
 
+class TicketDispatchTask:
+    # Periodically checks pending_tickets and sends tickets to ticket dispatchers
+    def __init__(self, interval: int) -> None:  # In deciseconds
+        self.interval = interval
+
+    async def write(self):
+        # Exceptions inside a client can stop the main event loop from working
+        # So handle exceptions here
+        while True:
+            log2(f"Checking if there are any undispatched tickets for roads")
+            for road, plates in pending_tickets.items():
+                try:
+                    while dispatchers.get(road):
+                        dispatcher = dispatchers[road].pop()
+                        if not dispatcher.closed:
+                            for plate, tickets in plates.items():
+                                while tickets:
+                                    ticket = tickets.pop()
+                                    # Check if a ticket for that day has not already been sent
+                                    if last_tickets.get(plate) is not None:
+                                        if ticket[0] in last_tickets[plate] or ticket[1] in last_tickets[plate]:
+                                            # A ticket was already sent for that day
+                                            continue
+                                        last_tickets[plate].add(ticket[0])
+                                        last_tickets[plate].add(ticket[1])
+                                    else:
+                                        last_tickets[plate] = {ticket[0], ticket[1]}
+                                    log2(f"Sending ticket day={ticket[0]}, {ticket[1]} for {plate}")
+                                    dispatcher.writer.write(ticket[2])
+                                    await dispatcher.writer.drain()
+                            dispatchers[road].append(dispatcher)
+                            break
+                        
+                except Exception as e:
+                    log2(
+                        "Error while sending ticket ... doing nothing for now",
+                        traceback.format_exc(),
+                    )
+                finally:
+                    # await self.client.close()
+                    pass
+            await asyncio.sleep(self.interval)
+
+# Handles a plate sent from a camera client
+class PlateHandler:
+    def __init__(self, client: Client) -> None:
+        self.client = client
+    
+    async def handle(self, parsed, road, mile, limit):
+        plate = parsed[0]
+        timestamp = parsed[1]
+        log(self.client.writer, f"Got Plate {{{plate} at {timestamp}}}")
+        plates[plate] = plates.get(plate, []) + [(mile, timestamp)]
+        plates[plate].sort()
+        log(self.client.writer, f"There is information about the client, checking if a ticket is needed")
+        for i in range(1, len(plates[plate])):
+            # Check adjacent observations
+            mile2 = plates[plate][i][0] 
+            mile1 = plates[plate][i-1][0]
+            timestamp2 = plates[plate][i][1] 
+            timestamp1 = plates[plate][i-1][1]
+            
+            if timestamp1 > timestamp2:
+                # Swap if the car is travelling in the opposite direction
+                mile1, mile2 = mile2, mile1
+                timestamp2, timestamp1 = timestamp1, timestamp2
+
+            distance =  abs(mile2 - mile1)
+            t = timestamp2 - timestamp1 
+            v = abs((distance/t)*60*60) # Convert miles/s to miles/h
+            day1 = math.floor(timestamp1 / 86400)
+            day2 = math.floor(timestamp2 / 86400)
+            log(self.client.writer, f"{plate} has a speed of {v}, limit = {limit} on road={road}")
+            if v - limit >= 0.5:
+                # The client was overspeeding
+                log3(self.client.writer, f"{plate} was overspeeding with speed {v} on road {road} day= {day1} and day={day2}, {mile1}: {timestamp1}   {mile2}: {timestamp2} {timestamp}")
+                # The day on which the ticke is going to be sent (i.e. the current timestamp)
+                ticket = (TicketPacker(self.client).pack(plate, road, mile1, timestamp1, mile2, timestamp2, v))
+                if pending_tickets.get(road) is None:
+                    pending_tickets[road] = dict()
+
+                if pending_tickets[road].get(plate) is None:
+                    pending_tickets[road][plate] = set()
+                pending_tickets[road][plate].add((day1, day2, ticket))
+
 class CameraHandler:
     def __init__(self, client: Client, heartbeat_set) -> None:
         self.reader = client.reader
@@ -221,7 +305,7 @@ class CameraHandler:
             # The client has identified itself as a camera
             # The client can send only WantHeartbeat and Plate messages
             opcode = await self.reader.read(1)
-            if not opcode;
+            if not opcode:
                 # The client disconnected
                 log(self.client.writer, "Disconnected")
                 return
@@ -233,67 +317,9 @@ class CameraHandler:
                 # Remove the heart beat reader from self.readers
                 # So that any more heart beat requests from this client will be an error
                 del self.readers[0x40]
-                await HeartBeatTask(self.client, parsed).start()
+                await HeartBeatTask(self.client, parsed).start() # type: ignore
             if opcode == 0x20:
-                plate = parsed[0]
-                timestamp = parsed[1]
-                log(self.client.writer, f"Got Plate {{{plate} at {timestamp}}}")
-                if plates.get(plate) is None:
-                    # This is the first plate we have received
-                    plates[plate] = [(mile, timestamp)]
-                else:
-                    plates[plate].append((mile, timestamp))
-                    plates[plate].sort()
-                    log(self.client.writer, f"There is information about the client, checking if a ticket is needed")
-                    for i in range(1, len(plates[plate])):
-                        # Check adjacent observations
-                        mile2 = plates[plate][i][0] 
-                        mile1 = plates[plate][i-1][0]
-                        timestamp2 = plates[plate][i][1] 
-                        timestamp1 = plates[plate][i-1][1]
-                        
-                        if timestamp1 > timestamp2:
-                            # Swap everything
-                            mile1, mile2 = mile2, mile1
-                            timestamp2, timestamp1 = timestamp1, timestamp2
-
-                        distance =  mile2 - mile1 
-                        t = timestamp2 - timestamp1 
-                        v = abs((distance/t)*60*60) # Convert miles/s to miles/h
-                        if v - limit >= 0.5:
-                            # The client was overspeeding
-                            log(self.client.writer, f"{plate} was overspeeding with speed {v} on road {road}")
-                            road_dispatchers = dispatchers.get(road)
-
-                            if road_dispatchers is None:
-                                # There are no active dispatchers for this road, wait for a one to connect
-                                log(self.client.writer, f"There are no active dispatchers for {road}, waiting to send ticket to {plate}")
-                                if tickets.get(road) is None:
-                                    tickets[road] = deque()
-                                tickets[road].append((plate, road, mile1, timestamp1, mile2, timestamp2, v))
-                            else:
-                                today = math.floor(timestamp / 86400)
-                                if last_ticket.get(plate, -1) == today:
-                                    # Do not send the ticket as we have already dispatched it
-                                    log(self.client.writer, f"A ticket was already sent to {plate} on {road} for this day {today}, last: {last_ticket.get(plate, -1)}")
-                                    pass
-                                else:
-                                    not_sent = True
-                                    for dispatcher in road_dispatchers:
-                                        if not dispatcher.closed:
-                                            # Send the ticket
-                                            last_ticket[plate] = today
-                                            log(self.client.writer, f"There was an active dispatcher, sending ticket to {plate}, {road}")
-                                            await TicketWriter(dispatcher).write(plate, road, mile1, timestamp1, mile2, timestamp2, v)
-                                            not_sent = False
-                                            break
-                                    if not_sent:
-                                        # There are no active dispatchers for this road, wait for a one to connect
-                                        log(self.client.writer, f"There are no open dispatchers for {road}, waiting to send ticket to {plate}")
-                                        if tickets.get(road) is None:
-                                            tickets[road] = deque()
-                                        tickets[road].append((plate, road, mile1, timestamp1, mile2, timestamp2, v))
-
+                await PlateHandler(self.client).handle(parsed, road, mile, limit)
 
 class DispatcherHandler:
     def __init__(self, client: Client, hearbeat_set) -> None:
@@ -307,47 +333,39 @@ class DispatcherHandler:
             self.readers = dict()
 
     async def handle(self):
-        log(self.client.writer, "The client has identifed itself as a dispatcher")
-        num_roads = await self.reader.readexactly(1)
-        num_roads = num_roads[0]
-        roads = await self.reader.read(2 * num_roads)
-        roads = struct.unpack(f"!{num_roads}H", roads)
-        for road in roads:
-            if not dispatchers.get(road):
-                dispatchers[road] = [self.client]
-            else:
-                dispatchers[road].append(self.client)
-            if tickets.get(road) is not None:
-                log(self.client.writer, "A pending ticket for this road was found, sending it")
-                # task = asyncio.create_task(TicketWriter(self.client).write(*tickets[road].pop()))
-                # self.tasks.add(task)
-                # task.add_done_callback(self.tasks.discard)
-                try:
-                    while len(tickets[road]) != 0:
-                        ticket = tickets[road].popleft()
-                        await TicketWriter(self.client).write(*ticket)
-                except:
-                    log(self.client.writer, "ERROR, couldn't send ticket", traceback.format_exc())
-
-        log(self.client.writer, f"Dispatcher {{roads: {roads}}}")
-        while True:
-            # The client has identified itself as a dispatcher
-            # The only valid client request is WantHeartbeat
-            opcode = await self.reader.read(1)
-            if opcode:
-                opcode = opcode[0]
-                instance = self.readers[opcode](self.client)
-                parsed = await instance.read()
-                if opcode == 0x40:
-                    # Remove the heart beat reader from self.readers
-                    # So that any more heart beat requests from this client will be an error
-                    del self.readers[0x40]
-                    await HeartBeatTask(self.client, parsed).start()
-            else:
-                # The client disconnected
-                log(self.client.writer, "Disconnected")
-                return
-
+        try:
+            log(self.client.writer, "The client has identifed itself as a dispatcher")
+            num_roads = await self.reader.readexactly(1)
+            num_roads = num_roads[0]
+            roads = await self.reader.read(2 * num_roads)
+            roads = struct.unpack(f"!{num_roads}H", roads)
+            for road in roads:
+                if not dispatchers.get(road):
+                    dispatchers[road] = [self.client]
+                else:
+                    dispatchers[road].append(self.client)
+            log(self.client.writer, f"Dispatcher {{roads: {roads}}}")
+            while True:
+                # The client has identified itself as a dispatcher
+                # The only valid client request is WantHeartbeat
+                opcode = await self.reader.read(1)
+                if opcode:
+                    opcode = opcode[0]
+                    instance = self.readers[opcode](self.client)
+                    parsed = await instance.read()
+                    if opcode == 0x40:
+                        # Remove the heart beat reader from self.readers
+                        # So that any more heart beat requests from this client will be an error
+                        del self.readers[0x40]
+                        await HeartBeatTask(self.client, parsed).start()
+                else:
+                    # The client disconnected
+                    log(self.client.writer, "Disconnected")
+                    return
+        finally:
+            await self.client.close()
+            # for road in roads: # type: ignore
+            #    dispatchers[road].discard(self.client)
 
 class SpeedDaemonServer:
     def __init__(self, loop) -> None:
@@ -375,6 +393,7 @@ class SpeedDaemonServer:
                     opcode = (await reader.read(1))[0]
 
                 instance = self.handlers[opcode](client, heartbeat_set)
+                
                 await instance.handle()
         except Exception as e:
             log(writer, f"ERROR: {traceback.format_exc()}")
@@ -386,8 +405,6 @@ class SpeedDaemonServer:
                 log(writer, "ERROR: error writing error message")
         finally:
             await client.close()
-
-
 async def main():
     loop = asyncio.get_running_loop()
     # Remove these in production
@@ -396,13 +413,16 @@ async def main():
     logging.getLogger("asyncio").setLevel(logging.DEBUG)
     # ======
     sds = SpeedDaemonServer(loop)
+    tasks = set()
+    # Every 1 seconds check for unsent tickets
+    task = loop.create_task(TicketDispatchTask(1).write())
+    tasks.add(task)
+    task.add_done_callback(tasks.discard)
     server = await asyncio.start_server(
         sds.handle_connection, host="0.0.0.0", port=8000
     )
     log2("Started server on 0.0.0.0:8000")
     async with server:
         await server.serve_forever()
-
-
 asyncio.run(main(), debug=True)
 # asyncio.run(main())
