0f72a7b50a94c88dde16fb37d9a51773429a4823
ananthvk <123456789987654321sr@gmail.com>
2023-11-24 04:10:21 +0530

    Implemented get,put

--- 09-job-centre.py
+++ 09-job-centre.py
@@ -1,4 +1,7 @@
 import jsonschema
+import heapq
+from jsonschema.exceptions import ValidationError
+from typing import Any, Dict, Tuple
 
 put_schema = {
     "$schema": "http://json-schema.org/draft-04/schema",
@@ -34,23 +37,99 @@ abort_delete_schema = {
     "type": "object",
     "title": "ABORT/DELETE request",
     "description": "A client has requested a job to be removed or aborted",
-    "properties":{
+    "properties": {
         "request": {"type": "string", "enum": ["abort", "delete"]},
-        "id":{"type":"number", "minimum": 0}
+        "id": {"type": "integer", "minimum": 0},
     },
     "required": ["request", "id"],
     "additionalProperties": False,
 }
 
-instance = {
-    "request": "put",
-    "queue": "queue1",
-    "job": {"title": "example-job"},
-    "pri": 123,
+request_schema = {
+    "type": "object",
+    "title": "Request",
+    "description": "Can be any request type",
+    "properties": {
+        "request": {"type": "string", "enum": ["get", "put", "abort", "delete"]},
+    },
+    "required": ["request"],
 }
-instance2={"request":"get","queues":["queue1"], "wait": True}
-instance3 = {"request":"delete","id":12345}
 
-jsonschema.validate(instance=instance, schema=put_schema)
-jsonschema.validate(instance=instance2, schema=get_schema)
-jsonschema.validate(instance=instance3, schema=abort_delete_schema)
+# This counter starts from one and is incremented after a job is added
+# Since this application uses asyncio with single threading, there is no need to lock
+# this counter.
+job_id_counter = 0
+
+
+class Job:
+    def __init__(
+        self, job_id: int, job: Dict[Any, Any], queue: str | None = None
+    ) -> None:
+        self.job_id = job_id
+        self.job = job
+        self.queue = queue
+
+    @staticmethod
+    def create(job: Dict[Any, Any], queue: str) -> "Job":
+        global job_id_counter  # Add this line
+        job_id_counter += 1
+        return Job(job_id_counter, job, queue)
+
+    def __str__(self) -> str:
+        return f"< Job {self.job_id} of {self.queue}>"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+# Associates a queue name (string) with a heap(implemented as a list)
+queues: Dict[str, list[Tuple[int, Job]]] = dict()
+
+
+def put(queue_name: str, job_dict: Dict[Any, Any], priority: int) -> Job:
+    """
+    Puts the job on the specified queue and returns the job object
+    """
+
+    if queue_name not in queues:
+        # Create an empty job queue
+        queues[queue_name] = []
+
+    job = Job.create(job_dict, queue_name)
+    # Invert the priorities so that the default heap behaves as a max heap
+    heapq.heappush(queues[queue_name], (-priority, job))
+    return job
+
+def get(queues_list: list[str]):
+    """
+    Returns the job with the highest priority among all the 
+    given queues. If no job is found, None is returned
+    """
+    if queues_list is None:
+        return None
+
+    highest_priority = -1
+    queue_with_hightest_priority = ""
+    for queue in queues_list:
+        if queue in queues:
+            heap = queues[queue] 
+            # Find the element with highest priority in this queue
+            priority = -heap[0][0]
+            queue_name = queue
+            if priority > highest_priority:
+                highest_priority = priority
+                queue_with_hightest_priority = queue_name
+    
+    if highest_priority == -1:
+        return None
+
+    # We have found the queue which has the highest priority element
+    # among the given queues.
+    heap = queues[queue_with_hightest_priority]
+    priority, job = heapq.heappop(heap)
+    return job
+
+put("test", {"key": "do this"}, 3)
+put("test", {"key value": "GOOGOG"}, 2)
+print(queues)
+print(get(["test"]))
